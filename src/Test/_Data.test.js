2fcd5e689fa52eb25dd8ac68ce616fe8e6581a3d
Trần Nhất Quang <trannhatquang2405@gmail.com>
2023-08-15 23:33:40 +0700

    test

---
+++ src/Test/_Data.test.js
@@ -0,0 +1,33 @@
+import { _saveQuestion, _saveQuestionAnswer } from "_DATA";
+
+describe('TEST_saveQuestion', () => {
+    it('will return saved question and all expected fields are populated', async () => {
+        let testData = { optionOneText: "optionOneText", optionTwoText: "optionTwoText", author: "sarahedo" }
+        let result = await _saveQuestion(testData)
+        expect(result).not.toBeNull()
+        expect(result.author).toEqual(testData.author)
+        expect(result.optionOne).not.toBeNull()
+        expect(result.optionOne.text).toEqual(testData.optionOneText)
+        expect(result.optionTwo).not.toBeNull()
+        expect(result.optionTwo.text).toEqual(testData.optionTwoText)
+    });
+
+    it('an error is returned if incorrect data is passed to the function.', async () => {
+        let testData = { optionOneText: "optionOneText", optionTwoText: "optionTwoText" }
+        await expect(_saveQuestion(testData)).rejects.toEqual(expect.any(Error));
+    });
+})
+
+describe('TEST_saveQuestionAnswer', () => {
+    it('true is returned when correctly formatted data is passed to the function', async () => {
+        let testData = { authedUser: "sarahedo", qid: "xj352vofupe1dqz9emx13r", answer: "optionOne" }
+        let result = await _saveQuestionAnswer(testData)
+        expect(result).toBe(undefined)
+
+    });
+
+    // it('an error is returned if incorrect data is passed to the function.', async () => {
+    //     let testData = { optionOneText: "optionOneText", optionTwoText: "optionTwoText" }
+    //     await expect(_saveQuestion(testData)).rejects.toEqual(expect.any(Error));
+    // });
+})
